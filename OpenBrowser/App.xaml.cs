a16ae9ecf8eff604b7c1f610fcc3437d7f58c19b
shotadft <fukashta@gmail.com>
2025-04-16 23:05:10 +0900

    プロジェクト ファイルを追加します。

---
+++ OpenBrowser/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Windows;
+
+namespace OpenBrowser
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string? AppName { get; } = Assembly.GetExecutingAssembly().GetName().Name;
+        public static string? AppVersion { get; } = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+    }
+}
---
+++ OpenBrowser/Windows/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace OpenBrowser.Windows.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _AppName = App.AppName ?? string.Empty;
+        public string AppName
+        {
+            get => _AppName;
+            private set
+            {
+                _AppName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
